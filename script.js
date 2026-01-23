ed84e942620f5a2a699ad02e416ca8c9629c0578
Antoine-Bizet <bizet.antoine.pro@gmail.com>
2026-01-23 15:51:53 +0000

    exo js DOM

--- script.js
+++ script.js
@@ -1,5 +1,71 @@
-let serveur = ["8.8.8.8", "1.1.1.1", "208.67.222.222", "192.168.1.1", "10.0.0.1"]
+// const maintitle = document.querySelectorAll("#main-title")
+// const textes = document.querySelectorAll(".texte")
+// const allLi = document.querySelectorAll("li")
 
-for (let i = 0; i < serveur.length; i++) {
-    console.log("Ping vers : " + serveur[i]);
-}
+// console.log(maintitle)
+// console.log(textes)
+// console.log(allLi)
+
+
+// const btnClick = document.querySelector("#btnClick")
+// const inputTxt = document.querySelector("#inputTxt")
+// const resulat = document.querySelector("#resultat")
+
+// btnClick.addEventListener('click', () => {
+//     console.log("Bouton cliqué !")
+// })
+
+// inputTxt.addEventListener('input' ,(e) => {
+//     resulat.textContent = e.target.value
+// })
+
+// btnClick.addEventListener('mouseenter' , () => {
+//     btnClick.style.backgroundColor = '#0300cc'
+// })
+
+// btnClick.addEventListener('mouseleave' , () => {
+//     btnClick.style.backgroundColor = '#cc0000'
+// })
+
+// const titre = document.querySelector('#title')
+// const zone = document.querySelector('#zone')
+// const btnAjoutez = document.querySelector('#add')
+// const btnSupprimez = document.querySelector('#remove')
+
+// titre.textContent = "Pk pas 2"
+
+// zone.innerHTML =
+// `
+// <ul>
+//     <li>Premier élément</li>
+//     <li>Deuxième élément</li>
+//     <li>Troisième élément</li>
+// </ul>
+// `
+
+// btnAjoutez.addEventListener('click' , () => {
+//     const p = document.createElement('p')
+//     p.textContent = "Nouveau"
+//     zone.appendChild(p)
+// })
+
+// btnSupprimez.addEventListener('click' , () => {
+//     const paragraphes = zone.querySelectorAll('p')
+//     if (paragraphes.length > 0) {
+//         paragraphes[paragraphes.length -1].remove()
+//     }
+// })
+
+btnClick.addEventListener('click', () => {
+    console.log("Bouton cliqué !")
+})
+
+inputTxt.addEventListener('input' ,(e) => {
+    resulat.textContent = e.target.value
+})
+
+`
+<ul>
+    <li>inputTxt</li>
+</ul>
+`
